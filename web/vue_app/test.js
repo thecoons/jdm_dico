80234770d4827c7bcc12d85698344c234691b75e
thecoon <antobarth@gmail.com>
2016-12-27 02:10:19 +0100

    test vue.js fonctionnel

--- web/vue_app/test.js
+++ web/vue_app/test.js
@@ -1,8 +1,16 @@
 Vue.component('test-item',{
-  template: '<p> Le module de template test </p>',
+  template: '<div class="row">\
+                <p> {{message}} </p>\
+                <div class="input-field col s3">\
+                  <input placeholder="Quelle mot?" id="word" type="text" class="validate" v-model="word">\
+                  <label for="word">Entrer un mot :</label>\
+                </div>\
+                <a class="waves-effect waves-light btn" @click="getTestApi">button</a>\
+              </div>',
   data: function(){
       return {
-        chart: {}
+        message: "hello",
+        word: ""
       }
     },
   computed: {
@@ -12,9 +20,16 @@ Vue.component('test-item',{
 
   },
   methods:{
-
+    getTestApi: function(){
+      var resource = this.$resource('/test{/word}')
+      resource.get({word:this.word}).then((data) => {
+        this.message = data.body
+      },(data) =>{
+        this.message = "Error loading data"
+      });
+    }
   },
   mounted: function(){
-
+    window.test = this
   }
 })
