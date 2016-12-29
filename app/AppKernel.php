0ca729e64a2b3b0e91a2070f0971a2fd1b32230e
thecoon <antobarth@gmail.com>
2016-12-29 18:18:36 +0100

    debug

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Neo4j\OGM\OGMBundle\Neo4jOGMBundle(),
+            // new Neo4j\OGM\OGMBundle\Neo4jOGMBundle(),
             new AppBundle\AppBundle(),
         ];
 
